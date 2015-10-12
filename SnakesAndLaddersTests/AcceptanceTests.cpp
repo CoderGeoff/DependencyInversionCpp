1e518a820ac5d590e286ebee1af5092257debe41
unknown <9uv9ot@GBCBRBONES01.net.plm.eds.com>
2015-10-12 21:44:44 +0530

    Remove acceptance tests

--- SnakesAndLaddersTests/AcceptanceTests.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include <memory>
-
-#include <gtest/gtest.h>
-
-TEST(AcceptanceTests, GivenAPlayerOnSquare1_WhenTheyTakeATurn_TheyShouldMoveForwardTheNumberShownOnTheDie)
-{
-
-}
