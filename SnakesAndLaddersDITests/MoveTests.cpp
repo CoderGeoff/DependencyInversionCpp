9c304f4b2136b34a0076a60662f746f17d837835
unknown <9uv9ot@GBCBRBONES01.net.plm.eds.com>
2015-10-12 23:47:45 +0530

    Simplify mock board

--- SnakesAndLaddersDITests/MoveTests.cpp
+++ SnakesAndLaddersDITests/MoveTests.cpp
@@ -7,23 +7,21 @@
 
 using namespace DependencyInversion;
 
-class BoardWithNoSnakes : public IBoard
+class BoardWith100SquaresAndNoSnakes : public IBoard
 {
     int m_LastSquare;
 public:
-    BoardWithNoSnakes(int numberOfSquares) : m_LastSquare(numberOfSquares - 1) {}
-
     bool TryGetSnakeWithHeadAt(int square, std::pair<int, int>* snake) const override
     {
         return false;
     }
-    virtual int LastSquare() const override { return m_LastSquare; }
+    virtual int LastSquare() const override { return 99; }
 };
 
 TEST(MoveTests, GivenAPlayerOnSquare97_WhenTheyMove3_TheyShouldEndUpOnSquare98)
 {
     // Given
-    auto board = std::make_shared<BoardWithNoSnakes>(100);
+    auto board = std::make_shared<BoardWith100SquaresAndNoSnakes>();
     auto iboard = std::static_pointer_cast<IBoard>(board);
     Move move(iboard);
 
