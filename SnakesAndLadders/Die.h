2325776f3a19f56b47ead9ae4c1ec864804e2983
unknown <9uv9ot@GBCBRBONES01.net.plm.eds.com>
2016-02-17 19:22:07 +0530

    Remove non DI version

--- SnakesAndLadders/Die.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-namespace WithoutDependencyInversion {
-class Die
-{
-public:
-    static int Throw();
-};
-}
--- SnakesAndLadders/Exports.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#ifdef SNAKESANDLADDERS_EXPORTS
-#    define SNAKESANDLADDERS_API __declspec(dllexport)
-#else
-#    define SNAKESANDLADDERS_API __declspec(dllimport)
-#endif
